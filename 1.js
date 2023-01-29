28adea9a77e570d7b9c343fe0758f295702d89ef
reshma <programmingwithreshma@gmail.com>
2023-01-29 15:25:40 +0530

    hsdj

---
+++ 1.js
@@ -0,0 +1,64 @@
+/*console.log("hello world")
+console.log("this is reshma ")
+var a =10;
+console.log("the value of a is :",a)
+a=7070
+console.log("the value of a is :",a)
+const b =40
+console.log("the value of b is :",b)
+//b=80
+//console.log("the value of b is :",b)
+
+var t;
+console.log("the data type of t is :", typeof t)
+
+t = "reshu";
+console.log("the data type of t is :", typeof t)
+
+t = 10;
+console.log("the data type of t is :", typeof t)
+
+t = true;
+console.log("the data type of t is :", typeof t)
+
+var a=10;
+var b=10;
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+var g = a % b;
+var h = a ** b;
+console.log(`the sum is : ${ c} the  diff is : ${d} the produt  is : ${e} the quotient is :${f} the reminder is : ${g} the exponent is : ${h}`)
+
+
+
+
+var a=230;
+var b=900;
+var c=23;
+if(a>b && a>c)
+{
+  console.log(`${a} is greatest among ${a} ,${b} and ${c} `);
+}
+else if (b>a && b>c)
+{
+  console.log(`${b} is greatest among ${a} , ${b} and ${c} `);
+}
+else
+{
+  console.log(`${c} is greatest among ${a} , ${b} and ${c} `);  
+}
+var d;
+d=( a>b ? (a>c?a:c) :(b>c?b:c))
+console.log(`${d} is greatest among ${a} ,${b} and ${c}`)
+*/
+var i=1
+while(i<5)
+{
+  console.log("sai aishu")
+  i++;
+}
+
+
+
